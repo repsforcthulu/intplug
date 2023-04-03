be312b14e231ab7264cf56041cdc38a17ea0cdea
repsforcthulu <evansbrett86@gmail.com>
2023-04-02 22:56:59 -0400

    changed gamespec to updated url, refactored

--- lib/intplug.rb
+++ lib/intplug.rb
@@ -4,8 +4,7 @@
 
 class Integer
   def range?(a, b) #checks if .self is between range a..b
-    arr = (a..b).to_a
-    if arr.include? self
+    if ((a..b).to_a).include? self
       true
     else
       false
